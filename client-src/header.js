e1989137df77dad8065de416b2af5d0d8538af58
Sivadass N <nsivadass@gmail.com>
2018-07-22 23:59:50 +0530

    Added header

---
+++ client-src/header.js
@@ -0,0 +1,11 @@
+class Heaader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDummyState: true
+    };
+  }
+  render() {
+    return <header>Header</header>;
+  }
+}
