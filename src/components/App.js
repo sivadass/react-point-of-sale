d3847f1c82a3b8ff9c909fc0e6b2660b3b9533af
Sivadass N <nsivadass@gmail.com>
2018-08-15 23:47:26 +0530

    Updated sass file names

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import 'styles/App.scss';
+import React from "react";
+import "styles/app.scss";
 
 const App = () => {
   return (
@@ -7,6 +7,6 @@ const App = () => {
       <h1>APP Started~~</h1>
     </div>
   );
-}
+};
 
 export default App;
