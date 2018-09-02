e7a3820d14f38cb0d519fffd4a5522221acc9824
Sivadass N <nsivadass@gmail.com>
2018-09-03 00:05:33 +0530

    Project scaffolding done

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/index.scss"
+
+const App = () => {
+  return (
+    <div className="app-wrapper">
+      <header className="container fluid">REACT POS</header>
+      <article className="container">
+        Welcome to React POS
+      </article>
+    </div>
+  );
+};
+
+export default App;
