d621635c8e0228eb52b48243cd5ab1e3a97195ce
Sivadass N <nsivadass@gmail.com>
2018-09-13 23:59:26 +0530

    Updated loading text

--- src/components/pages/products.js
+++ src/components/pages/products.js
@@ -2,7 +2,7 @@ import React from "react";
 import Service from "../../utils/service";
 
 class Products extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       loading: false,
@@ -10,67 +10,82 @@ class Products extends React.Component {
       pageNumber: 1,
       totalPages: 0,
       totalProducts: 0
-    }
+    };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getProucts();
   }
 
   nextPage = () => {
-    this.setState({
-      pageNumber: this.state.pageNumber + 1
-    }, () => {
-      this.getProucts();
-    })
-  }
+    this.setState(
+      {
+        pageNumber: this.state.pageNumber + 1
+      },
+      () => {
+        this.getProucts();
+      }
+    );
+  };
 
   getProucts = () => {
-    this.setState({loading: true});
-    Service.get(`https://pos.sivadass.in/wp-json/wc/v2/products?page=${this.state.pageNumber}`, (status, data, headers) => {
-      this.setState({loading: false});
-      console.log(headers);
-      if(status === 200){
-        this.setState({
-          products: data,
-          totalProducts: headers["x-wp-total"],
-          totalPages: headers["x-wp-totalpages"]
-        })
+    this.setState({ loading: true });
+    Service.get(
+      `https://pos.sivadass.in/wp-json/wc/v2/products?page=${
+        this.state.pageNumber
+      }`,
+      (status, data, headers) => {
+        this.setState({ loading: false });
+        console.log(headers);
+        if (status === 200) {
+          this.setState({
+            products: data,
+            totalProducts: headers["x-wp-total"],
+            totalPages: headers["x-wp-totalpages"]
+          });
+        }
       }
-    })
-  }
+    );
+  };
 
-  render(){
-    const {loading, products} = this.state;
-    if(loading){
-      return(
+  render() {
+    const { loading, products } = this.state;
+    if (loading) {
+      return (
         <div className="app-wrapper">
-          <p>Loading...</p>
+          <p>Loading products ...</p>
         </div>
-      )
+      );
     }
 
-    let renderProducts = products.map( (item) => {
-      return(
+    let renderProducts = products.map(item => {
+      return (
         <div className="product-item" key={item.id}>
           {item.images.length > 0 && <img src={item.images[0].src} />}
           <p>{item.name}</p>
           <h3>${item.price}</h3>
         </div>
-      )
-    })
+      );
+    });
     return (
       <div className="app-wrapper">
         <div className="products-container">
           {this.state.products.length > 0 && renderProducts}
         </div>
         <div className="pagination">
-          Showing {this.state.products.length} products out of {this.state.totalProducts} 
-          <a href="#" onClick={this.nextPage} disabled={this.state.totalPages === this.state.pageNumber}>More Products</a>
+          Showing {this.state.products.length} products out of{" "}
+          {this.state.totalProducts}
+          <a
+            href="#"
+            onClick={this.nextPage}
+            disabled={this.state.totalPages === this.state.pageNumber}
+          >
+            More Products
+          </a>
         </div>
       </div>
-    )
+    );
   }
-};
+}
 
 export default Products;
