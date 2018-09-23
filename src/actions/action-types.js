d79cfde493170940012c3cf022a91bea389bf9ce
Sivadass <sivadass@Sivadass-Macbook.local>
2018-09-24 00:01:34 +0530

    Added cart action types

--- src/actions/action-types.js
+++ src/actions/action-types.js
@@ -2,3 +2,6 @@ export const TOGGLE_MENU = "TOGGLE_MENU";
 export const TOGGLE_CART = "TOGGLE_CART";
 
 export const NETWORK_STATUS = "NETWORK_STATUS";
+
+export const ADD_TO_CART = "ADD_TO_CART";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
